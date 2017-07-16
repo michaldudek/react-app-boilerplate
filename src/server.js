a683c780bad1c954887d863d4c576ee46a9f501b
Michał Pałys-Dudek <michal@michaldudek.pl>
2017-07-16 02:17:11 +0100

    Cleanup of unneeded node modules etc.

--- src/server.js
+++ src/server.js
@@ -1,32 +1,14 @@
+import express from 'express'
+import nunjucks from 'nunjucks'
+
+import api from './api'
+import ssr from './ssr'
+
+import paths from '../build/js/paths'
+
 // simple flags
 const isProduction = process.env.NODE_ENV === 'production'
 const isDevelopment = !isProduction
-
-// also require paths
-const paths = require('../build/js/paths')
-
-// load and register babel to use all the fancy new js stuff
-require('babel-register')({
-  presets: ['es2015', 'react-app'],
-  ignore: paths.nodeModules,
-  plugins: [
-    ['css-modules-transform', {
-      generateScopedName: isProduction ? '[hash:base64:8]' : '[name]__[local]__[hash:base64:5]',
-      extensions: ['.css']
-    }],
-    ['module-resolver', {
-      root: [paths.src],
-      extensions: ['.js', '.css', '.json']
-    }]
-  ]
-})
-require('babel-polyfill')
-
-const express = require('express')
-const nunjucks = require('nunjucks')
-
-const api = require('./api').default
-const ssr = require('./ssr').default
 
 // initiate express app and configure it
 const app = express()
